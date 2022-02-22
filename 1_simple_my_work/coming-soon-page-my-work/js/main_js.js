c4d18a7abca76b8a118c56314257e5ecdfb6e684
Jason Cameron <dev.jasoncameron@gmail.com>
2022-02-21 22:23:09 -0600

    second challenge completed.  Working on this more tommorrow

--- 1_simple_my_work/coming-soon-page-my-work/js/main_js.js
+++ 1_simple_my_work/coming-soon-page-my-work/js/main_js.js
@@ -22,8 +22,8 @@ let x = setInterval(function() {
     let distance = countDownDate - now;
 
     // Time calculations for days, hours, minutes and seconds
-    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));                            //   Keeping the extra code
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));      // for future study and use
     let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((distance % (1000 * 60)) / (1000));
 
